5c8ec0d034ab801e91d95cb781fc2832309f1e76
suguna <96suguna@gmail.com>
2021-04-21 15:59:07 +0530

    added

--- empwage.sh
+++ empwage.sh
@@ -57,3 +57,20 @@ esac
 
 salary=$(( $employeeHrs*$employeeRate ))
 echo "Salary= $salary"
+isFullTime=1;
+isPartTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
+
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+        empCheck=$((RANDOM%3));
+                case $empCheck in
+                        $isFullTime)
+                empHrs=8
+                                        ;;
+                $isPartTime)
+                empHrs=4
+                                        ;;
+                *)
