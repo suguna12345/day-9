fc9e37fe8214ef428a302e9185d71c3a18ce9efa
suguna <96suguna@gmail.com>
2021-04-21 15:34:03 +0530

    added

--- empwage.sh
+++ empwage.sh
@@ -8,4 +8,15 @@ then
 else
    echo "Absent"
 fi
+isPresent=1;
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ];
+then
+   empRatePerHrs=20
+   empHrs=8
+   salary=$(($empHrs*$empRatePerHrs))
+else
+   salary=0
+fi
+echo $salary
 
