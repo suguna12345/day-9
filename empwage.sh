e39cc23735fb5241a3b9b6b19b048cfde9d900f7
suguna <96suguna@gmail.com>
2021-04-21 15:16:42 +0530

    added

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo welcome to empwage computation problem
+isPresent=1;
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ];
+then
+   echo "Present"
+else
+   echo "Absent"
+fi
 
