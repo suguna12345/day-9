42ae73cc1cc88a49286de8ecf35b5892785efa44
suguna <96suguna@gmail.com>
2021-04-21 15:52:31 +0530

    added

--- empwage.sh
+++ empwage.sh
@@ -39,3 +39,21 @@ else
    salary=0
 fi
 echo $salary
+isFullTime=1;
+isPartTime=2;
+employeeRate=20;
+randomCheck=$((RANDOM%3))
+case $randomCheck in
+   $isFullTime)
+      echo Fulltime
+      employeeHrs=8;;
+   $isPartTime)
+      echo Parttime
+      employeeHrs=4;;
+   *)
+      echo absent
+      employeeHrs=0;;
+esac
+
+salary=$(( $employeeHrs*$employeeRate ))
+echo "Salary= $salary"
