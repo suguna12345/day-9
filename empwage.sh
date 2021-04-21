7eb9f0b24e16c713ca1d3c3372f809c73b3a906c
suguna <96suguna@gmail.com>
2021-04-21 16:07:55 +0530

    added

--- empwage.sh
+++ empwage.sh
@@ -91,3 +91,21 @@ do
                 $IS_FULL_TIME)
                         empHrs=8
                                 ;;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHours=0;
+totalWorkingDays=0;
+
+function getWorkHrs() {
+        local $empCheck=$1
+        case $empCheck in
+        $IS_FULL_TIME)
+        empHrs=8
+                                        ;;
+        $IS_PART_TIME)
+        empHrs=4
+                                        ;;
