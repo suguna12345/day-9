4491561cfb79155a09bfb287c8716fb4d1af9e81
suguna <96suguna@gmail.com>
2021-04-21 16:03:57 +0530

    added

--- empwage.sh
+++ empwage.sh
@@ -74,3 +74,20 @@ do
                 empHrs=4
                                         ;;
                 *)
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKIN>
+do
+   ((totalWorkingDays++))
+   empCheck=$((RANDOM%3));
+        case $empCheck in
+                $IS_FULL_TIME)
+                        empHrs=8
+                                ;;
